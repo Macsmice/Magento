303d2993162a00b0c4b136f6002527caf69fb01d
Babs Gösgens <babsgosgens@gmail.com>
2011-09-10 16:16:51 +0200

    Added Joomla Block

---
+++ app/code/community/Crossinghippos/NookuServer/Block/Joomla.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Koowa Model
+ *
+ * @category    Crossinghippos
+ * @package     Crossinghippos_Koowa
+ * @author      Babs Gösgens/Crossing Hippos <babs@crossinghippos.nl>
+ * @copyright   Copyright (c) 2011 Crossing Hippos (http://crossinghippos.nk)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Crossinghippos_NookuServer_Block_Joomla extends Mage_Core_Block_Template
+{
+
+    protected $_state = array();
+
+    public function _construct()
+    {
+        parent::_construct();
+    }
+}
